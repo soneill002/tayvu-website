61881fa54551d203862dbc39fbe76ed619f836de
Mutiu Oyekunle <mutiuoyekunle@gmail.com>
2025-07-22 07:23:06 +0100

    fix: set Vite root to project dir; move index.html up

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+  // project root (where index.html lives)
+  root: '.',
+  // folder to copy as-is (images, favicon, etc.)
+  publicDir: 'public',
+  build: { outDir: 'dist' },
+  server: { port: 5173 }
+};
